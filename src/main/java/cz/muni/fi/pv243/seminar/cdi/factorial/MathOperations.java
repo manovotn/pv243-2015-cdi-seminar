jharting/a4m36jee-2014-cdi-seminar

[cdi-seminar] Initial version of cdi seminar

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/MathOperations.java
@@ -1,0 +1,31 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.factorial;
+
+import java.math.BigInteger;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class MathOperations {
+
+    /**
+     * Returns a result of multiplication of a sequence of (from * (from + 1) * (from + 2) ... (to -1) * to)
+     *
+     * @throws IllegalArgumentException if any of the parameters is less than or equal to zero or if the "to" parameter
+     *         is less then the "from" parameter
+     */
+    public BigInteger multiplySequence(long from, long to) {
+        if (from <= 0 || to <= 0) {
+            throw new IllegalArgumentException("Must be positive");
+        }
+        if (from > to) {
+            throw new IllegalArgumentException("from must be lower than to");
+        }
+
+        BigInteger result = BigInteger.ONE;
+        for (long i = from; i <= to; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        return result;
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/util/BigIntegerToByteArrayConverter.java
@@ -1,0 +1,31 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.factorial.util;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ * Converts a BigInteger to its byte representation.
+ *
+ * @author Jozef Hartinger
+ */
+@FacesConverter("bigIntegerByteArray")
+public class BigIntegerToByteArrayConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return BigInteger.valueOf(Long.valueOf(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if (!(value instanceof BigInteger)) {
+            throw new IllegalArgumentException("Expected an instance of BigInteger but received: " + value);
+        }
+        return Arrays.toString(BigInteger.class.cast(value).toByteArray());
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/util/FactorialComputationFinished.java
@@ -1,0 +1,27 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.factorial.util;
+
+import java.math.BigInteger;
+
+/**
+ * This class is used as an even payload that carries information about finished factorial computation.
+ *
+ * @author Jozef Hartinger
+ */
+public class FactorialComputationFinished {
+
+    private final long number;
+    private final BigInteger result;
+
+    public FactorialComputationFinished(long number, BigInteger result) {
+        this.number = number;
+        this.result = result;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public BigInteger getResult() {
+        return result;
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/view/FactorialForm.java
@@ -1,0 +1,34 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.factorial.view;
+
+import java.math.BigInteger;
+
+/**
+ * Backing bean for the factorial form (factorial.xhtml)
+ */
+// TODO: turn this class into a named bean with name "factorial"
+public class FactorialForm {
+
+    private Long input;
+    private BigInteger result;
+
+    public void compute() {
+        // TODO: implement using Factorial bean
+    }
+
+    public void reset() {
+        this.input = null;
+        this.result = null;
+    }
+
+    public Long getInput() {
+        return input;
+    }
+
+    public void setInput(Long input) {
+        this.input = input;
+    }
+
+    public BigInteger getResult() {
+        return result;
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/students/Country.java
@@ -1,0 +1,31 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.students;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public enum Country {
+
+    SLOVAKIA("Slovakia", "Bratislava", "Kosice", "Banska Bystrica", "Zilina", "Martin", "Prievidza", "Other"),
+    CZECH_REPUBLIC("Czech republic", "Praha", "Brno", "Ostrava", "Jihlava", "Plzen", "Other");
+
+    private final String label;
+    private final List<String> cities;
+
+    private Country(String label, String... cities) {
+        this.label = label;
+        List<String> tempCities = new ArrayList<>(cities.length);
+        for (String city : cities) {
+            tempCities.add(city);
+        }
+        this.cities = Collections.unmodifiableList(tempCities);
+    }
+
+    public List<String> getCities() {
+        return cities;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/students/StudentRegistrationWizard.java
@@ -1,0 +1,52 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.students;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Holds the state of the student registration process.
+ * @author Jozef Hartinger
+ *
+ */
+@ConversationScoped
+@Named
+@Stateful
+public class StudentRegistrationWizard {
+
+    @Inject
+    private Conversation conversation;
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    private final Student student = new Student();
+
+    @Produces
+    @Named
+    public Student getStudent() {
+        return student;
+    }
+
+    public void begin() {
+        if (conversation.isTransient()) {
+            conversation.begin();
+        }
+    }
+
+    public String cancel() {
+        conversation.end();
+        return "cancel";
+    }
+
+    public String register() {
+        manager.persist(student);
+        conversation.end();
+        return "register";
+    }
+}

src/test/java/cz/ctu/fee/a4m36jee/seminar/cdi/FactorialTest.java
@@ -1,0 +1,76 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi;
+
+import java.math.BigInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.Factorial;
+import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.util.FactorialComputationFinished;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Arquillian.class)
+public class FactorialTest {
+
+    @Inject
+    private Factorial factorial;
+
+    @Deployment
+    public static Archive<?> getDeployment() {
+        return ShrinkWrap
+                .create(WebArchive.class)
+                .addPackages(true, Factorial.class.getPackage())
+                .addClass(EventObserver.class);
+    }
+
+    @Test
+    public void testSerialFactorial() {
+        assertEquals(BigInteger.ONE, factorial.compute(0));
+        assertEquals(BigInteger.ONE, factorial.compute(1));
+        assertEquals(BigInteger.valueOf(2), factorial.compute(2));
+        assertEquals(BigInteger.valueOf(6), factorial.compute(3));
+        assertEquals(BigInteger.valueOf(24), factorial.compute(4));
+        assertEquals(BigInteger.valueOf(120), factorial.compute(5));
+        assertEquals(BigInteger.valueOf(3628800), factorial.compute(10));
+    }
+
+    @Test
+    @Ignore // TODO: remove once event notification is implemented
+    public void testEvent(EventObserver observer) {
+        observer.reset();
+        factorial.compute(6);
+        assertNotNull(observer.getEvent());
+        assertNotNull(observer.getEvent().getResult());
+        assertEquals(6, observer.getEvent().getNumber());
+        assertEquals(BigInteger.valueOf(720), observer.getEvent().getResult());
+    }
+
+    @ApplicationScoped
+    protected static class EventObserver {
+        private FactorialComputationFinished event;
+
+        public FactorialComputationFinished getEvent() {
+            return event;
+        }
+
+        public void reset() {
+            this.event = null;
+        }
+
+        public void observe(@Observes FactorialComputationFinished event) {
+            this.event = event;
+        }
+    }
+}

manovotn/pv243-2015-cdi-seminar

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/MathOperations.java
@@ -1,0 +1,31 @@
+package cz.muni.fi.pv243.seminar.cdi.factorial;
+
+import java.math.BigInteger;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class MathOperations {
+
+    /**
+     * Returns a result of multiplication of a sequence of (from * (from + 1) * (from + 2) ... (to -1) * to)
+     *
+     * @throws IllegalArgumentException if any of the parameters is less than or equal to zero or if the "to" parameter
+     *         is less then the "from" parameter
+     */
+    public BigInteger multiplySequence(long from, long to) {
+        if (from <= 0 || to <= 0) {
+            throw new IllegalArgumentException("Must be positive");
+        }
+        if (from > to) {
+            throw new IllegalArgumentException("from must be lower than to");
+        }
+
+        BigInteger result = BigInteger.ONE;
+        for (long i = from; i <= to; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        return result;
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/util/BigIntegerToByteArrayConverter.java
@@ -1,0 +1,31 @@
+package cz.muni.fi.pv243.seminar.cdi.factorial.util;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ * Converts a BigInteger to its byte representation.
+ *
+ * @author Jozef Hartinger
+ */
+@FacesConverter("bigIntegerByteArray")
+public class BigIntegerToByteArrayConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return BigInteger.valueOf(Long.valueOf(value));
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        if (!(value instanceof BigInteger)) {
+            throw new IllegalArgumentException("Expected an instance of BigInteger but received: " + value);
+        }
+        return Arrays.toString(BigInteger.class.cast(value).toByteArray());
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/util/FactorialComputationFinished.java
@@ -1,0 +1,27 @@
+package cz.muni.fi.pv243.seminar.cdi.factorial.util;
+
+import java.math.BigInteger;
+
+/**
+ * This class is used as an even payload that carries information about finished factorial computation.
+ *
+ * @author Jozef Hartinger
+ */
+public class FactorialComputationFinished {
+
+    private final long number;
+    private final BigInteger result;
+
+    public FactorialComputationFinished(long number, BigInteger result) {
+        this.number = number;
+        this.result = result;
+    }
+
+    public long getNumber() {
+        return number;
+    }
+
+    public BigInteger getResult() {
+        return result;
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/view/FactorialForm.java
@@ -1,0 +1,34 @@
+package cz.muni.fi.pv243.seminar.cdi.factorial.view;
+
+import java.math.BigInteger;
+
+/**
+ * Backing bean for the factorial form (factorial.xhtml)
+ */
+// TODO: turn this class into a named bean with name "factorial"
+public class FactorialForm {
+
+    private Long input;
+    private BigInteger result;
+
+    public void compute() {
+        // TODO: implement using Factorial bean
+    }
+
+    public void reset() {
+        this.input = null;
+        this.result = null;
+    }
+
+    public Long getInput() {
+        return input;
+    }
+
+    public void setInput(Long input) {
+        this.input = input;
+    }
+
+    public BigInteger getResult() {
+        return result;
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/cdi/students/Country.java
@@ -1,0 +1,31 @@
+package cz.muni.fi.pv243.seminar.cdi.students;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public enum Country {
+
+    SLOVAKIA("Slovakia", "Bratislava", "Kosice", "Banska Bystrica", "Zilina", "Martin", "Prievidza", "Other"),
+    CZECH_REPUBLIC("Czech republic", "Praha", "Brno", "Ostrava", "Jihlava", "Plzen", "Other");
+
+    private final String label;
+    private final List<String> cities;
+
+    private Country(String label, String... cities) {
+        this.label = label;
+        List<String> tempCities = new ArrayList<>(cities.length);
+        for (String city : cities) {
+            tempCities.add(city);
+        }
+        this.cities = Collections.unmodifiableList(tempCities);
+    }
+
+    public List<String> getCities() {
+        return cities;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}

src/main/java/cz/muni/fi/pv243/seminar/cdi/students/StudentRegistrationWizard.java
@@ -1,0 +1,52 @@
+package cz.muni.fi.pv243.seminar.cdi.students;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.Conversation;
+import javax.enterprise.context.ConversationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Holds the state of the student registration process.
+ * @author Jozef Hartinger
+ *
+ */
+@ConversationScoped
+@Named
+@Stateful
+public class StudentRegistrationWizard {
+
+    @Inject
+    private Conversation conversation;
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    private final Student student = new Student();
+
+    @Produces
+    @Named
+    public Student getStudent() {
+        return student;
+    }
+
+    public void begin() {
+        if (conversation.isTransient()) {
+            conversation.begin();
+        }
+    }
+
+    public String cancel() {
+        conversation.end();
+        return "cancel";
+    }
+
+    public String register() {
+        manager.persist(student);
+        conversation.end();
+        return "register";
+    }
+}

src/test/java/cz/muni/fi/pv243/FactorialTest.java
@@ -1,0 +1,76 @@
+package cz.muni.fi.pv243;
+
+import java.math.BigInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import cz.muni.fi.pv243.seminar.cdi.factorial.Factorial;
+import cz.muni.fi.pv243.seminar.cdi.factorial.util.FactorialComputationFinished;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Arquillian.class)
+public class FactorialTest {
+
+    @Inject
+    private Factorial factorial;
+
+    @Deployment
+    public static Archive<?> getDeployment() {
+        return ShrinkWrap
+                .create(WebArchive.class)
+                .addPackages(true, Factorial.class.getPackage())
+                .addClass(EventObserver.class);
+    }
+
+    @Test
+    public void testSerialFactorial() {
+        assertEquals(BigInteger.ONE, factorial.compute(0));
+        assertEquals(BigInteger.ONE, factorial.compute(1));
+        assertEquals(BigInteger.valueOf(2), factorial.compute(2));
+        assertEquals(BigInteger.valueOf(6), factorial.compute(3));
+        assertEquals(BigInteger.valueOf(24), factorial.compute(4));
+        assertEquals(BigInteger.valueOf(120), factorial.compute(5));
+        assertEquals(BigInteger.valueOf(3628800), factorial.compute(10));
+    }
+
+    @Test
+    @Ignore // TODO: remove once event notification is implemented
+    public void testEvent(EventObserver observer) {
+        observer.reset();
+        factorial.compute(6);
+        assertNotNull(observer.getEvent());
+        assertNotNull(observer.getEvent().getResult());
+        assertEquals(6, observer.getEvent().getNumber());
+        assertEquals(BigInteger.valueOf(720), observer.getEvent().getResult());
+    }
+
+    @ApplicationScoped
+    protected static class EventObserver {
+        private FactorialComputationFinished event;
+
+        public FactorialComputationFinished getEvent() {
+            return event;
+        }
+
+        public void reset() {
+            this.event = null;
+        }
+
+        public void observe(@Observes FactorialComputationFinished event) {
+            this.event = event;
+        }
+    }
+}