VaclavChalupa/a4m36jee-2014-cdi-seminar

[cdi-seminar] Add CacheAwareFactorialDecorator
 - Factorial computation use cache for results

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/CacheAwareFactorialDecorator.java
@@ -1,0 +1,40 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.factorial;
+
+import java.math.BigInteger;
+
+import javax.annotation.Priority;
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+import javax.interceptor.Interceptor;
+
+/**
+ * Decorator that checks the {@link FactorialCache} before initiating computation.
+ * If the result is found in cache, it is returned.
+ *
+ * @author Jozef Hartinger
+ */
+@Decorator
+@Dependent
+@Priority(Interceptor.Priority.APPLICATION)
+public class CacheAwareFactorialDecorator implements Factorial {
+
+    @Inject
+    @Delegate
+    @Any
+    private Factorial delegate;
+
+    @Inject
+    private FactorialCache cache;
+
+    @Override
+    public BigInteger compute(long number) {
+        BigInteger result = cache.get(number);
+        if (result == null) {
+            result = delegate.compute(number);
+        }
+        return result;
+    }
+}

manovotn/pv243-2015-cdi-seminar

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/CacheAwareFactorialDecorator.java
@@ -1,0 +1,38 @@
+package cz.muni.fi.pv243.seminar.cdi.factorial;
+
+import java.math.BigInteger;
+
+import javax.annotation.Priority;
+import javax.decorator.Decorator;
+import javax.decorator.Delegate;
+import javax.enterprise.inject.Any;
+import javax.inject.Inject;
+import javax.interceptor.Interceptor;
+
+/**
+ * Decorator that checks the {@link FactorialCache} before initiating computation.
+ * If the result is found in cache, it is returned.
+ *
+ * @author Jozef Hartinger
+ */
+@Decorator
+@Priority(Interceptor.Priority.APPLICATION)
+public class CacheAwareFactorialDecorator implements Factorial {
+
+    @Inject
+    @Delegate
+    @Any
+    private Factorial delegate;
+
+    @Inject
+    private FactorialCache cache;
+
+    @Override
+    public BigInteger compute(long number) {
+        BigInteger result = cache.get(number);
+        if (result == null) {
+            result = delegate.compute(number);
+        }
+        return result;
+    }
+}