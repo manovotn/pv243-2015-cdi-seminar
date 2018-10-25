VaclavChalupa/a4m36jee-2014-cdi-seminar

[cdi-seminar] Add implementation of the Factorial interface
 - A simple CDI bean with an injection point and scope declaration

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/FactorialImpl.java
@@ -1,0 +1,18 @@
+package cz.ctu.fee.a4m36jee.seminar.cdi.factorial;
+
+import java.math.BigInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class FactorialImpl implements Factorial {
+
+    @Inject
+    private MathOperations math;
+
+    @Override
+    public BigInteger compute(long number) {
+        return number == 0 ? BigInteger.ONE : math.multiplySequence(1, number);
+    }
+}

manovotn/pv243-2015-cdi-seminar

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/FactorialImpl.java
@@ -1,0 +1,18 @@
+package cz.muni.fi.pv243.seminar.cdi.factorial;
+
+import java.math.BigInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class FactorialImpl implements Factorial {
+
+    @Inject
+    private MathOperations math;
+
+    @Override
+    public BigInteger compute(long number) {
+        return number == 0 ? BigInteger.ONE : math.multiplySequence(1, number);
+    }
+}