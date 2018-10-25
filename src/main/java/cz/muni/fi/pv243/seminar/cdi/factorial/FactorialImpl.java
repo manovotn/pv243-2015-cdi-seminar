VaclavChalupa/a4m36jee-2014-cdi-seminar

[cdi-seminar] Add support for FactorialComputationFinished event

src/main/java/cz/ctu/fee/a4m36jee/seminar/cdi/factorial/FactorialImpl.java
@@ -3,7 +3,10 @@ package cz.ctu.fee.a4m36jee.seminar.cdi.factorial;
 import java.math.BigInteger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
+
+import cz.ctu.fee.a4m36jee.seminar.cdi.factorial.util.FactorialComputationFinished;
 
 @ApplicationScoped
 public class FactorialImpl implements Factorial {
@@ -11,8 +14,18 @@ public class FactorialImpl implements Factorial {
     @Inject
     private MathOperations math;
 
+    @Inject
+    private Event<FactorialComputationFinished> event;
+
     @Override
     public BigInteger compute(long number) {
-        return number == 0 ? BigInteger.ONE : math.multiplySequence(1, number);
+        BigInteger result = number == 0 ? BigInteger.ONE : math.multiplySequence(1, number);
+        fireEvent(number, result);
+        return result;
+    }
+
+    protected BigInteger fireEvent(long number, BigInteger result) {
+        event.fire(new FactorialComputationFinished(number, result));
+        return result;
     }
 }

manovotn/pv243-2015-cdi-seminar

src/main/java/cz/muni/fi/pv243/seminar/cdi/factorial/FactorialImpl.java
@@ -3,7 +3,10 @@ package cz.muni.fi.pv243.seminar.cdi.factorial;
 import java.math.BigInteger;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
+
+import cz.muni.fi.pv243.seminar.cdi.factorial.util.FactorialComputationFinished;
 
 @ApplicationScoped
 public class FactorialImpl implements Factorial {
@@ -11,8 +14,18 @@ public class FactorialImpl implements Factorial {
     @Inject
     private MathOperations math;
 
+    @Inject
+    private Event<FactorialComputationFinished> event;
+
     @Override
     public BigInteger compute(long number) {
-        return number == 0 ? BigInteger.ONE : math.multiplySequence(1, number);
+        BigInteger result = number == 0 ? BigInteger.ONE : math.multiplySequence(1, number);
+        fireEvent(number, result);
+        return result;
+    }
+
+    protected BigInteger fireEvent(long number, BigInteger result) {
+        event.fire(new FactorialComputationFinished(number, result));
+        return result;
     }
 }